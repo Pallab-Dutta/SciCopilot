35af79a2b19f290bcce6312671213b6e5ac6b688
Pallab Dutta <pallab9997@gmail.com>
2026-06-03 15:56:18 +0530

    update the app homepage, and privacy policy for scico_search

---
+++ scico_search/config.js
@@ -0,0 +1,8 @@
+// SciCo-Search Landing Page Configuration
+window.CONFIG = {
+    // Replace with your Google OAuth Client ID
+    clientId: "6789575941-b4nk3gck9gsoh9e5c8cfsnv6jbctd0r1.apps.googleusercontent.com",
+    
+    // Replace with your production Streamlit app URL (MUST include a trailing slash)
+    redirectUri: "https://scico-search.streamlit.app/"
+};
